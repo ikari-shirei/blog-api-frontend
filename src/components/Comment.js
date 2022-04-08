05453482b296a66347563d977a4e13a9c8855dc8
ikari-shirei <algrsyu@protonmail.com>
2022-04-08 11:43:26 +0300

    Convert message timestamp to date

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import '../styles/Comment.scss'
+import { DateTime } from 'luxon'
 
+// Components
 import Likes from './small/Likes'
 
 function Comment({ username, date, message }) {
+  const convertDate = (date) => {
+    const newDate = new Date(date)
+
+    return DateTime.fromJSDate(newDate).toLocaleString(DateTime.DATE_FULL)
+  }
   return (
     <div className="Comment">
       <div className="comment-top">
         <p className="comment-username">{username}</p>
-        <p className="comment-date">{date}</p>
+        <p className="comment-date">{convertDate(date)}</p>
       </div>
 
       <div className="comment-middle">
